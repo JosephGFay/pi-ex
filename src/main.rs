d622354d1299b305b1e0a530801d2709bb462e63
Joseph <joegfay@gmail.com>
2023-02-21 13:51:23 -0600

    added print statements

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use gpio::{GpioIn, GpioOut};
 
 fn main() {
+    println!("Starting pi-ex");
     let mut buzzer_pin = gpio::sysfs::SysFsGpioOutput::open(36).unwrap();
-
+    println!("Pin Set");
     buzzer_pin.set_high().expect("Failed to set high");
+    println!("Program finished without issue.");
 }
