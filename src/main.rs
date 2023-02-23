4ec96ca186c8968a93062c1afeec9007df6b59db
Joseph <tehbuildar@gmail.com>
2023-02-23 17:35:43 -0600

    using gpiozero

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use rust_gpiozero::*;
 
 fn main() {
+    println!("Starting blind software..");
     // Create a new LED attached to Pin 17
     let mut led = LED::new(3);
 
 // blink the LED
 // on_time: 2 seconds and off_time: 3 seconds
+
+
     led.blink(2.0,3.0);
+    println!("Blinked");
 }
