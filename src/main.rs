3ac03548fbed80d967fde3c47561490dcee19ecb
Joseph <tehbuildar@gmail.com>
2023-02-23 18:07:55 -0600

    added blink

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 extern crate rust_gpiozero;
 use rust_gpiozero::*;
-
+use std::thread::sleep;
+use std::time::Duration;
 fn main() {
     println!("Starting blink software..");
     // Create a new LED attached to Pin 17
     let mut led = LED::new(17);
 
-// blink the LED
-// on_time: 2 seconds and off_time: 3 seconds
+    loop{
+        // Make the led switch on
+        led.on();
 
+        // Let the LED stay on for one second
+        sleep(Duration::from_secs(1));
 
-    led.blink(0.2, 0.3);
-    led.set_blink_count(10);
-    println!("Blink ran");
+        // Make the led switch off
+        led.off();
+
+        // Let the LED stay off for one second
+        sleep(Duration::from_secs(1));
+        println!("Blinked");
+
+    }
 }
+
